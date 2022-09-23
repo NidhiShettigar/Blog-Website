5310170b1ae4e4070f765a03b4cc7ff17da4cab0
Nidhi Shettigar <shettigarnidhi813@gmail.com>
2022-09-23 23:08:30 +0530

    database connected

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import json
+from datetime import datetime
 
+
+with open('templates/config.json', 'r') as c:
+    params = json.load(c) ["params"]
+
+app = Flask(__name__)
+local_server = True
+if (local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
+db = SQLAlchemy(app)
+
+class Contacts(db.Model): 						
+    srno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    phone_number = db.Column(db.String(12), nullable=False)
+    message = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
 
 @app.route("/")
 def home():
@@ -12,8 +34,17 @@ def about():
     return render_template('about.html')
 
 
-@app.route("/contact")
+@app.route("/contact", methods = ['GET','POST'])
 def contact():
+    if (request.method == 'POST'):
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        msg = request.form.get('message')
+
+        entry = Contacts(name=name, email=email, phone_number=phone,message=msg, date=datetime.now())
+        db.session.add(entry)
+        db.session.commit()
     return render_template('contact.html')
 
 
